f6cd195fe057bc756bb9695dd726cf8b4b56ade2
beamatos03 <bea.matos1978@gmail.com>
2023-04-11 09:07:47 -0300

    alguns erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConversorComponent } from './conversor/conversor.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./conversor.component.css']
 })
 export class ConversorComponent {
+  moeda : number = 0;
+  valor : number = 0;
+ resultado : string = ""
 
+
+
+ moedas(value: string){
+  switch(value) {
+    case "CAD":
+       // if modo 1 is selected do something.
+       break;
+    case "MXN":
+       // if modo 2 is selected do something.
+       break;
+    case "KRW":
+       // if modo 3 is selected do something.
+       break;
+  }
 }
