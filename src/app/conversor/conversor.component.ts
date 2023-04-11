7bf8c3b7e430295ab10529c154a2098f3a1e47cf
beamatos03 <bea.matos1978@gmail.com>
2023-04-11 09:55:43 -0300

    quarto commit

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -8,21 +8,31 @@ import { Component } from '@angular/core';
 export class ConversorComponent {
   moeda : number = 0;
   valor : number = 0;
+  cambio: number = 0;
  escolha : string = "";
  resultado : string = "";
 
 
 
- moedas(){
-  switch(this.escolha) {
-    case "CAD":
-       this.resultado = this.escolha;
-       break;
-    case "MXN":
-      this.resultado = this.escolha;
-       break;
-    case "KRW":
-      this.resultado = this.escolha;
-       break;
-  }
- }}
+
+ calculaCAD(){
+  var resultado = this.valor/this.cambio;
+  this.resultado = "O resultado da conversão de BRL para Dolares canadenses é: CAD " + resultado.toFixed(2);
+ }
+
+ calculaKRW(){
+  var resultado = this.valor/this.cambio;
+  this.resultado = "O resultado da conversão de BRL para Wons Sul Coreanos é: WON " + resultado.toFixed(2);
+ }
+
+ calculaBRL(){
+  var resultado = this.valor/this.cambio;
+  this.resultado = "O resultado da conversão de BRL para Dolares canadenses é: CAD " + resultado.toFixed(2);
+
+ }
+   
+
+
+}
+
+
